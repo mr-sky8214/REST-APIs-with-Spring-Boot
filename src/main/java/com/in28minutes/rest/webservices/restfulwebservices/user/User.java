db5c9ef5217021656896410b584eb7dadc6a3c0b
Akash Kamble <kambleakash8214@gmail.com>
2024-01-06 20:09:40 +0530

    Added static filtering using @JsonIgnore and @JsonIgnoreProperties to SomeBean class

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,6 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -9,9 +10,11 @@ public class User {
     private Integer id;
 
     @Size(min = 2, message = "Name should have atleast 2 characters")
+    @JsonProperty("user_name")
     private String name;
 
     @Past(message = "Birth date should be in past")
+    @JsonProperty("birth_date")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
